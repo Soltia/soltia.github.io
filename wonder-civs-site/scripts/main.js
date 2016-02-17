a3bfecb1d67228503dce68ad2a637f9fad6c21cc
Rafael Araújo <rafaelfsd.araujo@gmail.com>
2016-02-17 10:20:41 -0200

    'commit'

---
+++ wonder-civs-site/scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/estrela.png');
+    } else {
+      myImage.setAttribute ('src','images/draco.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'At his worst behavior -> ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'At his worst behavior -> ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
